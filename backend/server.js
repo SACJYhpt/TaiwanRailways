9b7b644a5ce2f51b8811c10bb1548d533fdb289e
SACJYhpt <jerry10710@gmail.com>
2025-12-31 02:44:52 +0800

    嘗試解決時間額外時差問題

--- backend/server.js
+++ backend/server.js
@@ -132,11 +132,20 @@ function addMinutes(isoTimeStr, minutes) {
         return isoTimeStr;
     }
 
-    let adjustedTime = new Date(date.getTime() + (minutes * 60 * 1000));
-    
-    let taiwanTime = new Date(adjustedTime.getTime() + (8 * 60 * 60 * 1000));
-    
-    return taiwanTime.toISOString().replace('Z', '');
+    data.setTime(date.getTime() + (minutes * 60 * 1000));
+
+    let year = date.getFullYear();
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let day = String(date.getDate()).padStart(2, '0');
+    let hr = String(date.getHours()).padStart(2, '0');
+    let min = String(date.getMinutes()).padStart(2, '0');
+    let sec = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hr}:${min}:${sec}`;
+
+    // let adjustedTime = new Date(date.getTime() + (minutes * 60 * 1000));
+    // let taiwanTime = new Date(adjustedTime.getTime() + (8 * 60 * 60 * 1000));
+    // return taiwanTime.toISOString().replace('Z', '');
 }
 
 function findJoint(station){
